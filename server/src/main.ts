8a68c769486724e12ac8c127c14f68920949097b
webmihail <donskoymihail@gmail.com>
2021-12-05 00:07:56 +0200

    Added client and server modules

---
+++ server/src/main.ts
@@ -0,0 +1,13 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  try {
+    const PORT = process.env.PORT || 5000;
+    const app = await NestFactory.create(AppModule);
+    await app.listen(PORT, () => {
+      console.log(`server started on port: ${PORT}`);
+    });
+  } catch (e) {}
+}
+bootstrap();
