c13b479f0678b71d9e575fda2b8cfc2978de53f7
webmihail <donskoymihail@gmail.com>
2021-12-08 10:26:06 +0200

    configured docker-compose with makefile for pg database and created postgres connection

---
+++ server/src/dbConnect/dbConnect.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as typeOrmConfig from '../../ormconfig';
+
+@Module({
+  imports: [TypeOrmModule.forRoot(typeOrmConfig as TypeOrmModuleOptions)],
+})
+export class DbConnect {}
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
   try {
     const PORT = process.env.PORT || 5000;
     const app = await NestFactory.create(AppModule);
+    app.enableCors();
+    app.setGlobalPrefix('/api/v1');
+
+    //Helmet can help protect your app from some well-known (https://github.com/helmetjs/helmet#how-it-works)
+    app.use(helmet());
+
     await app.listen(PORT, () => {
       console.log(`server started on port: ${PORT}`);
     });
