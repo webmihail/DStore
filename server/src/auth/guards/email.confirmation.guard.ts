9b3c57cba7eb1d2462b0881df0f6e7b60d0f3f22
webmihail <donskoymihail@gmail.com>
2021-12-26 12:08:42 +0200

    added email confirmation guard

---
+++ server/src/auth/guards/email.confirmation.guard.ts
@@ -0,0 +1,20 @@
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { RequestWithUserDTO } from '../dtos/user.request.dto';
+
+@Injectable()
+export class EmailConfirmationGuard implements CanActivate {
+  canActivate(context: ExecutionContext) {
+    const request: RequestWithUserDTO = context.switchToHttp().getRequest();
+
+    if (!request.user?.isEmailConfirmed) {
+      throw new UnauthorizedException('Confirm your email first');
+    }
+
+    return true;
+  }
+}
