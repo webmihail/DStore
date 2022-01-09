2edebc2364a8f854cdf3515e7fa48071d0d7cb09
webmihail <donskoymihail@gmail.com>
2022-01-09 23:13:11 +0200

    added brand controllers

--- server/src/brands/brands.controller.ts
+++ server/src/brands/brands.controller.ts
@@ -1,4 +1,104 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  UseGuards,
+} from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import JwtAuthGuard from 'src/auth/guards/jwt.auth.guard';
+import { BanGuard } from 'src/bans/guards/ban.guard';
+import { Permissions } from 'src/permissions/constants';
+import PermissionGuard from 'src/permissions/guards/permission.guard';
+import { DeleteResult } from 'typeorm';
+import { BrandsService } from './brands.service';
+import { BrandDTO } from './dtos/brand.dto';
+import { BrandEditDTO } from './dtos/brand.edit.dto';
+import { BrandCreateDTO } from './dtos/brend.create.dto';
+import { Brand } from './entity/brand.entity';
 
+@ApiTags('Brands')
 @Controller('brands')
-export class BrandsController {}
+export class BrandsController {
+  constructor(private readonly brandsServices: BrandsService) {}
+
+  @UseGuards(
+    PermissionGuard([
+      Permissions.SubscriptionFullManagement,
+      Permissions.SubscriptionCategoryProductManagementRead,
+    ]),
+  )
+  @UseGuards(BanGuard)
+  @UseGuards(JwtAuthGuard)
+  @ApiOperation({ summary: 'Get all brands' })
+  @ApiResponse({ status: 200, type: [Brand] })
+  @Get()
+  async getAllBrands(): Promise<BrandDTO[]> {
+    return await this.brandsServices.getAll();
+  }
+
+  @UseGuards(
+    PermissionGuard([
+      Permissions.SubscriptionFullManagement,
+      Permissions.SubscriptionCategoryProductManagementRead,
+    ]),
+  )
+  @UseGuards(BanGuard)
+  @UseGuards(JwtAuthGuard)
+  @ApiOperation({ summary: 'Get brand by id' })
+  @ApiResponse({ status: 200, type: Brand })
+  @Get(':id')
+  async getBrand(@Param('id') id: string): Promise<BrandDTO> {
+    return await this.brandsServices.getById(id);
+  }
+
+  @ApiOperation({ summary: 'Create brand' })
+  @ApiResponse({ status: 200, type: Brand })
+  @UseGuards(
+    PermissionGuard([
+      Permissions.SubscriptionFullManagement,
+      Permissions.SubscriptionCategoryProductManagementWrite,
+    ]),
+  )
+  @UseGuards(BanGuard)
+  @UseGuards(JwtAuthGuard)
+  @Post()
+  async createBrand(@Body() data: BrandCreateDTO): Promise<BrandDTO> {
+    return await this.brandsServices.create(data);
+  }
+
+  @ApiOperation({ summary: 'Update brand' })
+  @ApiResponse({ status: 200, type: Brand })
+  @UseGuards(
+    PermissionGuard([
+      Permissions.SubscriptionFullManagement,
+      Permissions.SubscriptionCategoryProductManagementWrite,
+    ]),
+  )
+  @UseGuards(BanGuard)
+  @UseGuards(JwtAuthGuard)
+  @Put(':id')
+  async updateBrand(
+    @Param('id') id: string,
+    @Body() data: BrandEditDTO,
+  ): Promise<BrandDTO> {
+    return await this.brandsServices.update(id, data);
+  }
+
+  @ApiOperation({ summary: 'Delete brand' })
+  @UseGuards(
+    PermissionGuard([
+      Permissions.SubscriptionFullManagement,
+      Permissions.SubscriptionCategoryProductManagementWrite,
+    ]),
+  )
+  @UseGuards(BanGuard)
+  @UseGuards(JwtAuthGuard)
+  @Delete(':id')
+  async deleteBrand(@Param('id') id: string): Promise<DeleteResult> {
+    return await this.brandsServices.delete(id);
+  }
+}
