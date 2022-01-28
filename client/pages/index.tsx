e4ff34de5a2870011960ba9ac9fac2d23d009341
webmihail <donskoymihail@gmail.com>
2022-01-28 20:15:54 +0200

    changed store module

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 
-const Main: NextPage = () => {
+const Home: NextPage = () => {
   return <div>Main Next Page</div>;
 };
 
-export default Main;
+export default Home;
--- client/store/index.ts
+++ client/store/index.ts
@@ -1,29 +1,18 @@
 import createSagaMiddleware from "@redux-saga/core";
-import {
-  applyMiddleware,
-  compose,
-  configureStore,
-  Store,
-} from "@reduxjs/toolkit";
+import { configureStore, Store } from "@reduxjs/toolkit";
 import { Context, createWrapper, MakeStore } from "next-redux-wrapper";
-import { composeWithDevTools } from "redux-devtools-extension";
 import { reducer, rootReducer } from "./reducers";
 import { rootSaga } from "./sagas";
 
-declare global {
-  interface Window {
-    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
-  }
-}
-
 const sagaMiddleware = createSagaMiddleware();
 
-const enhancer =
-  process.env.NODE_ENV === "production"
-    ? compose(applyMiddleware(sagaMiddleware))
-    : composeWithDevTools(applyMiddleware(sagaMiddleware));
+const store = configureStore({
+  reducer,
+  devTools: process.env.NODE_ENV === "production" ? false : true,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(sagaMiddleware),
+});
 
-const store = configureStore({ reducer, enhancers: [enhancer] });
 const makeStore: MakeStore<Store<RootState>> = (context: Context) => store;
 
 sagaMiddleware.run(rootSaga);
