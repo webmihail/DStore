594ad906ce877da92ee2610efe124af761cf98de
webmihail <donskoymihail@gmail.com>
2022-01-31 17:47:19 +0200

    added colors and global styles

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,9 +1,11 @@
-import React, {FC} from 'react';
-import {AppProps} from 'next/app';
-import { wrapper } from '../store';
+import React, { FC } from "react";
+import { AppProps } from "next/app";
+import { wrapper } from "../store";
+import "antd/dist/antd.css";
+import "../styles/root.scss";
 
-const WrappedApp: FC<AppProps> = ({Component, pageProps}) => (
-    <Component {...pageProps} />
+const WrappedApp: FC<AppProps> = ({ Component, pageProps }) => (
+  <Component {...pageProps} />
 );
 
 export default wrapper.withRedux(WrappedApp);
